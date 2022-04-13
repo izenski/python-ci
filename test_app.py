58d45d6969582eff6b728414a0023ead56cfd164
Steve <stevealbers@hotmail.com>
2022-04-13 07:32:33 +0000

    update app to use click

--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
-from app import hello 
+from app import change
 
-def test_hello():
-    assert "bob" in hello("bob")
+
+def test_quarters():
+    assert({5: "quarters"} in change(1.25))
+
+def test_change():
+    assert [{5: "quarters"}, {1: "nickels"}, {4: "pennies"}] == change(1.34)
