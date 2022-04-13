4746a1e80c5fbadcf1a6d1f623f6e2a535f3f0f4
Steve <stevealbers@hotmail.com>
2022-04-13 07:46:02 +0000

    run black formatting

--- test_app.py
+++ test_app.py
@@ -2,7 +2,8 @@ from app import change
 
 
 def test_quarters():
-    assert({5: "quarters"} in change(1.25))
+    assert {5: "quarters"} in change(1.25)
+
 
 def test_change():
     assert [{5: "quarters"}, {1: "nickels"}, {4: "pennies"}] == change(1.34)
